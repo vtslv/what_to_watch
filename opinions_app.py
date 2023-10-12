0824d2fd17dddf73c7166bb46487038cb74d46e4
Vtslv D <vstolopov@yandex.ru>
2023-10-12 22:42:30 +0300

    add flash-message

--- opinions_app.py
+++ opinions_app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from random import randrange
 
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
@@ -75,6 +75,13 @@ def add_opinion_view():
     form = OpinionForm()
     # Если ошибок не возникло, то
     if form.validate_on_submit():
+        text = form.text.data
+        # Если в БД уже есть мнение с текстом, который ввёл пользователь,
+        if Opinion.query.filter_by(text=text).first() is not None:
+            # вызвать функцию flash и передать соответствующее сообщение
+            flash('Такое мнение уже было оставлено ранее!')
+            # и вернуть пользователя на страницу «Добавить новое мнение»
+            return render_template('add_opinion.html', form=form)
         # нужно создать новый экземпляр класса Opinion
         opinion = Opinion(
             title=form.title.data,
